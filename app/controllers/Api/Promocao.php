e1aa7564a6920127a7e426f7a6b12a12deb9dc1a
Igor Cacerez <igor.cacerez@gmail.com>
2020-05-14 14:48:47 -0300

    Finalização API

--- app/controllers/Api/Promocao.php
+++ app/controllers/Api/Promocao.php
@@ -12,6 +12,7 @@ namespace Controller\Api;
 // Importação
 use Model\Usuario;
 use Sistema\Controller;
+use Sistema\Helper\File;
 use Sistema\Helper\Input;
 use Sistema\Helper\Seguranca;
 
@@ -153,30 +154,300 @@ class Promocao extends Controller
     } // End >> fun::getAll()
 
 
-
-
+    /**
+     * Método responsável por cadastrar uma nova promoção no
+     * banco de dados.
+     * ------------------------------------------------------
+     * @url api/promocao/insert
+     * @method POST
+     */
     public function insert()
     {
         // Variaveis
         $dados = null;
         $usuario = null;
         $post = null;
+        $caminho = null;
+        $arquivo = null;
 
+        // Busca o usuário logado
+        $usuario = $this->objSeguranca->security();
+
+        // Recupera os dados post
+        $post = $_POST;
+
+        // Verifica se é associado
+        if($usuario->nivel == "associado")
+        {
+            // Salva os dados padroes
+            $post["id_usuario"] = $usuario->id_usuario;
+            $post["status"] = "analise";
+        }
+
+        // Verifica se informou os dados obrigatórios
+        if(!empty($post["nome"]) &&
+            !empty($post["id_usuario"]) &&
+            !empty($post["id_categoria"]) &&
+            !empty($post["valor_antigo"]) &&
+            !empty($post["valor"]) &&
+            !empty($post["link"]) &&
+            !empty($post["descricao"]) &&
+            !empty($post["data_validade"]))
+        {
+            // Verifica se informou a imagem
+            if($_FILES["arquivo"]["size"] > 0)
+            {
+                // Caminho
+                $caminho = "./storage/promocao/";
+
+                // Instancia o objeto de upload
+                $objUpload = new File();
+
+                // Configurações
+                $objUpload->setStorange($caminho);
+                $objUpload->setMaxSize(2 * MB);
+                $objUpload->setExtensaoValida(["jpg","png","jpeg"]);
+                $objUpload->setFile($_FILES);
+
+                // Verifica se o tamanho é permitido
+                if($objUpload->validaSize())
+                {
+                    // Verifica se a extensão é válida
+                    if($objUpload->validaExtensao())
+                    {
+                        // Realiza o upload
+                        $arquivo = $objUpload->upload();
+
+                        // Verifica se deu certo
+                        if($arquivo != false)
+                        {
+                            // Add o arquivo a ser salvo
+                            $post["imagem"] = $arquivo;
+
+                            // Adiciona a promocao
+                            $obj = $this->objModelPromocao->insert($post);
+
+                            // Verifica se salvou
+                            if(!empty($obj))
+                            {
+                                // Busca a promocao
+                                $obj = $this->objModelPromocao
+                                    ->get(["id_promocao" => $obj])
+                                    ->fetch(\PDO::FETCH_OBJ);
+
+                                // Array de retorno
+                                $dados = [
+                                    "tipo" => true,
+                                    "code" => 200,
+                                    "objeto" => $obj,
+                                    "mensagem" => "Promoção adicionada com sucesso."
+                                ];
+                            }
+                            else
+                            {
+                                // Deleta a imagem
+                                unlink($caminho . $arquivo);
+
+                                // Avisa que deu erro
+                                $dados = ["mensagem" => "Ocorreu um erro ao salvar promocao."];
+                            } // Error >> Ocorreu um erro ao salvar promocao
+                        }
+                        else
+                        {
+                            // Msg
+                            $dados = ["mensagem" => "Ocorreu um erro ao realizar o upload da imagem."];
+                        } // Error >> Ocorreu um erro ao realizar o upload da imagem.
+                    }
+                    else
+                    {
+                        // Msg
+                        $dados = ["mensagem" => "A extensão da imagem não é suportada."];
+                    } // Error >> A extensão da imagem não é suportada.
+                }
+                else
+                {
+                    // Msg
+                    $dados = ["mensagem" => "O tamanho da imagem é maior que 2MB."];
+                } // Error >> O tamanho da imagem é maior que 2MB
+            }
+            else
+            {
+                // Msg
+                $dados = ["mensagem" => "Imagem não informada."];
+            } // Error >> Imagem não informada.
+        }
+        else
+        {
+            // Msg
+            $dados = ["mensagem" => "Campos obrigatórios não informados."];
+        } // Error >> Campos obrigatórios não informados.
+
+        // Retorno
+        $this->api($dados);
 
     } // End >> fun::insert()
 
 
 
-    public function update()
+    /**
+     * Método responsável por alterar os dados de uma
+     * determinada promoção.
+     * ------------------------------------------------------
+     * @param $id [Id Promoção]
+     * ------------------------------------------------------
+     * @url api/promocao/update/[ID]
+     * @method PUT
+     */
+    public function update($id)
     {
-        //
+        // Variaveis
+        $dados = null;
+        $usuario = null;
+        $obj = null;
+        $objAlterado = null;
+
+        // Verifica se está logado
+        $usuario = $this->objSeguranca->security();
+
+        // Recupera os dados put
+        $put = $this->objInput->put();
+
+        // Recupera os dados do objeto
+        $obj = $this->objModelPromocao
+            ->get(["id_promocao" => $id])
+            ->fetch(\PDO::FETCH_OBJ);
+
+        // Verifica existe
+        if(!empty($obj))
+        {
+            // Verifica se possui permissão
+            if($usuario->nivel == "admin" || $obj->id_usuario == $usuario->id_usuario)
+            {
+                // Verifica se não é admin
+                if($usuario->nivel != "admin")
+                {
+                    // Verifica se o status é diferente de cancelado ou analise
+                    if(!empty($put["status"]) && $put["status"] != "cancelado" && $put["status"] != "analise")
+                    {
+                        // Deleta o status
+                        unset($put["status"]);
+                    }
+                }
+
+                // Verifica se vai alterar algo
+                if(!empty($put))
+                {
+                    // Altera e verifica se alterou
+                    if($this->objModelPromocao->update($put, ["id_promocao" => $id]) != false)
+                    {
+                        // Busca o objeto alterado
+                        $objAlterado = $this->objModelPromocao
+                            ->get(["id_promocao" => $id])
+                            ->fetch(\PDO::FETCH_OBJ);
+
+                        // Retorna
+                        $dados = [
+                            "tipo" => true,
+                            "code" => 200,
+                            "mensagem" => "Informações alteradas com sucesso.",
+                            "objeto" => [
+                                "antes" => $obj,
+                                "atual" => $objAlterado
+                            ]
+                        ];
+                    }
+                    else
+                    {
+                        // Msg
+                        $dados = ["mensagem" => "Ocorreu um erro ao alterar as informações."];
+                    } // Error >> Ocorreu um erro ao alterar as informações
+                }
+                else
+                {
+                    // Msg
+                    $dados = ["mensagem" => "Nada está sendo alterado"];
+                } // Error >> Nada está sendo alterado
+            }
+            else
+            {
+                // Msg
+                $dados = ["mensagem" => "Usuário sem permissão."];
+            } // Error >> Usuário sem permissão.
+        }
+        else
+        {
+            // Msg
+            $dados = ["mensagem" => "A promoção informada não foi encontrada."];
+        } // Error >> Promoção não encontrada
+
+        // Retorno
+        $this->api($dados);
 
     } // End >> fun::update()
 
 
-    public function delete()
+
+    /**
+     * Método responsável por deletar uma determinada promoção
+     * verificando se o usuário possui permissão para isso.
+     * ------------------------------------------------------
+     * @param $id [Id da promoção]
+     * ------------------------------------------------------
+     * @url api/promocao/delete/[ID]
+     * @method DELETE
+     */
+    public function delete($id)
     {
-        //
+        // Variaveis
+        $dados = null;
+        $usuario = null;
+        $obj = null;
+
+        // Verifica se está logado
+        $usuario = $this->objSeguranca->security();
+
+        // Recupera o objeto
+        $obj = $this->objModelPromocao
+            ->get(["id_promocao" => $id])
+            ->fetch(\PDO::FETCH_OBJ);
+
+        // Verifica se ele existe
+        if(!empty($obj))
+        {
+            // Verifica se o usuário possui permissão
+            if($usuario->nivel == "admin" || $usuario->id_usuario == $obj->id_usuario)
+            {
+                // Deleta
+                if($this->objModelPromocao->delete(["id_promocao" => $id]) != false)
+                {
+                    // Informa que deu certo
+                    $dados = [
+                        "tipo" => true,
+                        "code" => 200,
+                        "mensagem" => "Promoção deletada com sucesso.",
+                        "objeto" => $obj
+                    ];
+                }
+                else
+                {
+                    // Msg
+                    $dados = ["mensagem" => "Ocorreu um erro ao deletar a promoção."];
+                } // Error >> Ocorreu um erro ao deletar a promoção.
+            }
+            else
+            {
+                // Msg
+                $dados = ["mensagem" => "Usuário sem permissão."];
+            } // Error >> Usuário sem permissão.
+        }
+        else
+        {
+            // Msg
+            $dados = ["mensagem" => "Promoção não encontrada."];
+        } // Error >> Promoção não encontrado.
+
+        // Retorno
+        $this->api($dados);
 
     } // End >> fun::delete()
 
